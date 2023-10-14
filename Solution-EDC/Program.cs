b8b006010552d9a3bb4265f19b2f2e3b1a842a0d
Lucas Lalumiere <lucas.lalum@gmail.com>
2023-10-13 22:40:03 -0400

    Started Introduction screen

--- Solution-EDC/Program.cs
+++ Solution-EDC/Program.cs
@@ -1,11 +1,34 @@
-﻿
+﻿using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+
 namespace Solution_EDC
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Introduction();
+        }
+
+        public static void Introduction()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(@"
+░█████╗░██╗░░░██╗██████╗░██████╗░░░░░░░███████╗███████╗██████╗░░█████╗░
+██╔══██╗██║░░░██║██╔══██╗██╔══██╗░░░░░░╚════██║██╔════╝██╔══██╗██╔══██╗
+██║░░╚═╝██║░░░██║██████╔╝██████╔╝█████╗░░███╔═╝█████╗░░██████╔╝██║░░██║
+██║░░██╗██║░░░██║██╔═══╝░██╔═══╝░╚════╝██╔══╝░░██╔══╝░░██╔══██╗██║░░██║
+╚█████╔╝╚██████╔╝██║░░░░░██║░░░░░░░░░░░███████╗███████╗██║░░██║╚█████╔╝
+░╚════╝░░╚═════╝░╚═╝░░░░░╚═╝░░░░░░░░░░░╚══════╝╚══════╝╚═╝░░╚═╝░╚════╝░");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine(@"Welcome to Cupp-Zero.
+Your best friend for generating complex yet memorable passwords.
+
+Press any key to beggin:");
+
+            Console.ReadKey(true);
+            Console.Clear();
         }
     }
 }
