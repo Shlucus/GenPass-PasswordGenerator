8fbb8a83d621ec10b0dbe34c585609537134167c
Zachary Dubé <2273406@johnabbottcollege.net>
2023-10-14 00:04:10 -0400

    After merging im making this commit that should be selectvowel function

--- Solution-EDC/Program.cs
+++ Solution-EDC/Program.cs
@@ -5,18 +5,28 @@
 
         public static void Main(string[] args)
         {
-         
+        
         }
 
         public static string SelectVowel(string userInput) //return should be "ae";
         {
-            
+        
             string foundVowels = "";
             if (string.IsNullOrEmpty(userInput))
             {
                 return "Please enter a valid input";
             }
 
+            Console.WriteLine("MOUAHAHAHAHA");
+
+            foreach (char character in userInput.ToUpper())
+            {
+                if (character == 'A' || character == 'E' || character == 'O')
+                {
+                    foundVowels += character;
+                }
+            }
+            return foundVowels;
         }
 
     }
